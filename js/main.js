95a4cc783a419cb68e719d7943b580eabd99e273
Josue <jovz1798@gmail.com>
2020-12-05 15:06:32 -0600

    menu navigation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 const toggle = document.querySelector('#menu-check');
 let menu = document.querySelector('#menu');
 
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const projects = document.querySelector('#projects');
+const contact = document.querySelector('#contact');
+
+const home_section = document.querySelector('header');
+const about_section = document.querySelector('#about-section');
+const projects_section = document.querySelector('#projects-section');
+const contact_section = document.querySelector('#contact-section');
+const to_work = document.querySelector('#to-work');
+const to_home = document.querySelector('#arrow-home');
+
+
+home.addEventListener('click', ()=>{
+    home_section.scrollIntoView({ behavior: "smooth" });
+});
+
+about.addEventListener('click', ()=>{
+    about_section.scrollIntoView({ behavior: "smooth" });
+});
+
+projects.addEventListener('click', ()=>{
+    projects_section.scrollIntoView({ behavior: "smooth" });
+});
+
+contact.addEventListener('click', ()=>{
+    contact_section.scrollIntoView({ behavior: "smooth" });
+});
+
+to_work.addEventListener('click', ()=>{
+    projects_section.scrollIntoView({ behavior: "smooth" });
+});
+
+to_home.addEventListener('click', ()=>{
+    home_section.scrollIntoView({ behavior: "smooth" });
+});
+
 toggle.addEventListener('change', ()=> {
     if( toggle.checked){
         menu.style = 'animation: menu-in 400ms ease-in-out forwards';
